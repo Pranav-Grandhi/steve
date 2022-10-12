30168840d961ef1210cf6010fbf350a8d7b6f18a
Pranav Grandhi <pranavgrandhi@Pranavs-MacBook-Pro.local>
2022-10-12 08:48:45 -0700

    lazy load preserved routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,12 @@ import { lazy, Suspense } from "react";
 import { Routes as RouterRoutes, Route } from "react-router-dom";
 
 // Auto generates routes from files under ./pages // https://vitejs.dev/guide/features.html#glob-import
-const PRESERVED = import.meta.globEager("./pages/404.jsx");
+const PRESERVED = import.meta.glob("./pages/404.jsx");
 const ROUTES = import.meta.glob("./pages/**/[a-z[]*.jsx");
 
 const preserved = Object.keys(PRESERVED).reduce((preserved, file) => {
   const key = file.replace(/\.\/pages\/|\.jsx$/g, "");
-  return { ...preserved, [key]: PRESERVED[file].default };
+  return { ...preserved, [key]: lazy(PRESERVED[file]) };
 }, {});
 
 const routes = Object.keys(ROUTES).map((route) => {
