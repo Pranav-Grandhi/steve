e5bc96931ff8ef03db817d6e87ddbc2e4e5a6531
Pranav Grandhi <pranavgrandhi@pranavs-mbp.lan>
2022-10-11 22:53:50 -0700

    404 support

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,13 @@ import { lazy, Suspense } from "react";
 import { Routes as RouterRoutes, Route } from "react-router-dom";
 
 // Auto generates routes from files under ./pages // https://vitejs.dev/guide/features.html#glob-import
+const PRESERVED = import.meta.globEager("./pages/404.jsx");
 const ROUTES = import.meta.glob("./pages/**/[a-z[]*.jsx");
+
+const preserved = Object.keys(PRESERVED).reduce((preserved, file) => {
+  const key = file.replace(/\.\/pages\/|\.jsx$/g, '')
+  return { ...preserved, [key]: PRESERVED[file].default }
+}, {})
 
 const routes = Object.keys(ROUTES).map((route) => {
   const path = route.replace(/\.\/pages|index|\.jsx$/g, "");
@@ -11,18 +17,18 @@ const routes = Object.keys(ROUTES).map((route) => {
 });
 
 export function Routes() {
+  const NotFound = preserved?.["404"];
+
   return (
     <>
-      {" "}
       <Suspense fallback={"Loading..."}>
-        {" "}
         <RouterRoutes>
-          {" "}
           {routes.map(({ path, component: Component }) => {
             return <Route key={path} path={path} element={<Component />} />;
           })}{" "}
-        </RouterRoutes>{" "}
-      </Suspense>{" "}
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
+      </Suspense>
     </>
   );
 }
